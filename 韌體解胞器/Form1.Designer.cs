434f4dc50e804ef64d70dc80dcd9473f990a2fca
xpoi5010 <xpoi5010@gmail.com>
2017-03-05 14:38:53 +0800

    1.00ver

---
+++ 韌體解胞器/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace 韌體解胞器
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(232, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "請選擇transfer_list與system_new_file所在目錄";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(14, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(230, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(250, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(28, 22);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(14, 52);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(264, 26);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "開始轉換!";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 84);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(264, 46);
+            this.textBox3.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 164);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(264, 26);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "TEST";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(164, 12);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "原作者:xpirt - luxi78 - howellzhu";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 12);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "改寫C#:Yuan";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 118);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(264, 12);
+            this.progressBar1.TabIndex = 11;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 52);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(264, 26);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "停止轉換!";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(205, 146);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 12);
+            this.label4.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(290, 163);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button4);
+            this.Name = "Form1";
+            this.Text = "dat轉ext4軟體";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ 韌體解胞器/Form1.cs
@@ -0,0 +1,342 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+namespace 韌體解胞器
+{
+    public partial class Form1 : Form
+    {
+        bool enable = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string pr = "";
+        private void print(string input)
+        {
+            
+            System.Diagnostics.Debug.Print(input);
+        }
+        int version = 0;
+        int new_blocks = 0;
+        System.IO.TextReader trans_list;
+        System.IO.FileStream output_img;
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (!(textBox1.Text != "" & System.IO.File.Exists(textBox1.Text + @"\system.new.dat") & System.IO.File.Exists(textBox1.Text + @"\system.transfer.list")))
+            {
+                textBox3.Text = "錯誤:請確認您已經選擇目錄，且目錄內有system.transfer.list與system.new.dat檔案。";
+            }
+            else
+            {
+            backgroundWorker1.RunWorkerAsync();
+            backgroundWorker1.WorkerReportsProgress = true;
+            backgroundWorker1.ProgressChanged += BackgroundWorker1_ProgressChanged;
+                backgroundWorker1.RunWorkerCompleted += BackgroundWorker1_RunWorkerCompleted;
+                button1.Enabled = false;
+                button3.Visible = false;
+                button4.Visible = true;
+                enable = true;
+                textBox3.Text = "開始轉換";
+                commands.Clear();
+            }
+                
+        }
+
+        private void BackgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (!enable)
+            {
+                
+            }
+            else
+            {
+                
+            }
+        }
+
+        private void BackgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+
+            progressBar1.Value = e.ProgressPercentage;
+            label4.Text = pr;
+            textBox3.Text = "目前進度:" + e.ProgressPercentage.ToString() + " %" + System.Environment.NewLine;
+            if (e.ProgressPercentage >= 98)
+            {
+
+                
+                {
+                    textBox3.Text = "完成輸出，輸出位置:" + textBox1.Text + @"\system.img.ext4";
+                    button1.Enabled = true;
+                    progressBar1.Value = 0;
+                    button3.Visible = true;
+                    button4.Visible = false;
+                    
+                    print(enable.ToString());
+                }
+            }
+            else
+            {
+                if (!enable)
+                {
+                 print(enable.ToString());
+                textBox3.Text = "使用者手動停止了這個操作。";
+                button1.Enabled = true;
+                progressBar1.Value = 0;
+                button3.Visible = true;
+                button4.Visible = false;
+                System.IO.File.Delete(textBox1.Text + @"\system.img.ext4");
+                }
+                
+            }
+        }
+        private void Start()
+        {
+            
+        }
+        List<object[]> commands = new List<object[]>();
+        private void parse_transfer_list_file()
+        {
+            trans_list = new System.IO.StreamReader(textBox1.Text + @"\system.transfer.list");
+            version = Convert.ToInt32(trans_list.ReadLine());
+            new_blocks = Convert.ToInt32(trans_list.ReadLine());
+            if (version >= 2)
+            {
+                trans_list.ReadLine();
+                trans_list.ReadLine();
+            }
+            
+            for(int i = 4; i <= 6;i++)
+            {
+                string[] line = System.Text.RegularExpressions.Regex.Split(trans_list.ReadLine()," ");
+                string cmd = line[0];
+                if (cmd == "erase" | cmd == "new" | cmd == "zero")
+                {
+                    object[] temp = new object[2] { cmd, rangeset(line[1]) };
+                    commands.Add(temp);
+                }
+                
+            }
+            trans_list.Close();
+        }
+        private byte[] Read(System.IO.FileStream input,int count)
+        {
+            List<byte> tmp = new List<byte>();
+            for(int i = 1; i <= count; i++)
+            {
+                tmp.Add((byte)input.ReadByte());
+            }
+            return tmp.ToArray();
+        }
+        private List<int[]> rangeset(string src)
+        {
+            string[] src_set = System.Text.RegularExpressions.Regex.Split(src, ",");
+            List<int> num_set = new List<int>();
+            List<int[]> Return = new List<int[]>();
+            foreach(string item in src_set)
+            {
+                num_set.Add(Convert.ToInt32(item));
+            }
+            if(num_set.Count != num_set[0] + 1)
+            {
+                MessageBox.Show("Error.");
+            }
+            foreach (int i in Range(1, num_set.Count, 2))
+            {
+                int[] temp = new int[2] { num_set[i], num_set[i + 1] };
+                Return.Add(temp);
+            }
+            return Return;
+
+        }
+        private int[] Range(int start,int stop,int step)
+        {
+            int nowint = start;
+            List<int> uu = new List<int>();
+            uu.Add(start);
+            for(int i =0; ; i++)
+            {
+                nowint += step;
+                if (nowint == stop)
+                {
+                    break;
+                }
+                
+                uu.Add(nowint);
+            }
+            return uu.ToArray();
+        }
+       private int Max(List<int> input)
+        {
+            int max = 0;
+            foreach(int i in input)
+            {
+                if(max < i)
+                {
+                    max = i;
+                }
+            }
+            return max;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if(fbd.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int[] p = Range(0, 10, 2);
+            System.Diagnostics.Debug.WriteLine(Range(0, 10, 2));
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            if (textBox1.Text != "" & System.IO.File.Exists(textBox1.Text + @"\system.new.dat") & System.IO.File.Exists(textBox1.Text + @"\system.transfer.list"))
+            {
+                parse_transfer_list_file();
+                string z = "韌體:Android";
+                switch (version)
+                {
+                    case 1:
+                        print(z + "5.0");
+                       pr= (z + "5.0");
+                        break;
+                    case 2:
+                        print(z + "5.1");
+                        pr= (z + "5.1");
+                        break;
+                    case 3:
+                        print(z + "6.0");
+                        pr= (z + "6.0");
+                        break;
+                    case 4:
+                        print(z + "7.0");
+                        pr= (z + "7.0");
+                        break;
+                }
+
+                if (System.IO.File.Exists(textBox1.Text + @"\system.img.ext4"))
+                {
+                    System.IO.File.Delete(textBox1.Text + @"\system.img.ext4");
+                }
+                output_img = new System.IO.FileStream(textBox1.Text + @"\system.img.ext4", System.IO.FileMode.Create);
+                System.IO.FileStream new_data_file = new System.IO.FileStream(textBox1.Text + @"\system.new.dat", System.IO.FileMode.Open, System.IO.FileAccess.Read);
+                List<int[]> all_block_sets = new List<int[]>();
+                foreach (object[] command in commands)
+                {
+                    object[] tmp = command;
+                    List<int[]> temp = (List<int[]>)tmp[1];
+                    foreach (int[] i in temp)
+                    {
+                        all_block_sets.Add(i);
+                    }
+
+                }
+                List<int> tmp1 = new List<int>();
+                foreach (int[] pair in all_block_sets)
+                {
+                    tmp1.Add(pair[1]);
+                }
+                int max_file_size = Max(tmp1) * 4096;
+                //取得檔案大小
+                long maxsize = 0;
+                {
+                    foreach(object[] command in commands)
+                    {
+                        string n = (string)command[0];
+                        if (n == "new")
+                        {
+                            List<int[]> aa = (List<int[]>)command[1];
+                            int[] ll = aa[aa.Count - 1];
+                            long begin = ll[0];
+                            long end = ll[1];
+                            long block_count = end - begin;
+                            maxsize = (begin + block_count) * 4096;
+
+                        }
+                    }
+                    
+                }
+                System.Diagnostics.Debug.Print(maxsize.ToString());
+
+                foreach (object[] command in commands)
+                {
+                    string n = (string)command[0];
+                    if (n == "new")
+                    {
+                        
+                        List<int[]> tmpp = (List<int[]>)command[1];
+                            foreach (int[] block in tmpp)
+                            {
+                            if (enable == true) { 
+                                long begin = block[0];
+                                long end = block[1];
+                                long block_count = end - begin;
+                                long BLOCK_SIZE = 4096;
+                                print(String.Format("Copying {0} blocks into position {1}...", block_count, begin));
+                                //try
+                                //{
+                                //output_img.Seek(begin * 4096, System.IO.SeekOrigin.Begin);
+                                output_img.Position = begin * BLOCK_SIZE;
+                                // }
+                                //catch(Exception ex)
+                                //{
+                                //print("發生錯誤:" + ex.Message + "，在複製" + String.Format("位置{1}的{0} 個區塊時所發生的。", block_count, begin));
+                                //}
+
+
+
+
+                                while (block_count > 0)
+                                {
+
+                                    for (int i = 1; i <= 4096; i++)
+                                    {
+                                        output_img.WriteByte((byte)new_data_file.ReadByte());
+
+                                    }
+                                    // output_img.Write(Read(new_data_file,4096),0,1);
+
+                                    block_count -= 1;
+
+
+
+                                }
+                                Double process = Convert.ToDouble((begin + block_count) * 4096) / Convert.ToDouble(maxsize) * Convert.ToDouble(100);
+                                backgroundWorker1.ReportProgress(Convert.ToInt32(process));
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                    }
+                }
+                output_img.Close();
+                new_data_file.Close();
+                print("輸出ext4檔案位置:" + textBox1.Text + @"\system.img.ext4");
+                
+            }
+            else
+            {
+                print("錯誤:請確認您已經選擇目錄，且目錄內有system.transfer.list與system.new.dat檔案。");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            enable = false;
+        }
+    }
+}
